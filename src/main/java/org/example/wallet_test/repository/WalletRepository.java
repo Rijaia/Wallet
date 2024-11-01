167824745d9f319340582cf451af9e611d4e49fb
Rijaia <zaya_89@mail.ru>
2024-11-01 13:54:55 +0300

    Step two. Service, controller, repository. Empty

---
+++ src/main/java/org/example/wallet_test/repository/WalletRepository.java
@@ -0,0 +1,7 @@
+package org.example.wallet_test.repository;
+
+import org.example.wallet_test.model.Wallet;
+import org.springframework.data.repository.Repository;
+
+public interface WalletRepository extends Repository<Wallet, Long> {
+}
